9790cc29483865cd9fe31afc4d8d927775d1bdb4
Sofiene Benamar <Sofiene.benamar@hotmail.com>
2024-05-13 14:47:16 +0200

    User Model

---
+++ src/accounts/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from src import bcrypt, db
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(db.integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    created_on = db.Columb(db.DateTime, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, email, password, is_admin=False):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+        self.created_on = datetime.now()
+        self.is_admin = is_admin
+
+    def __repr__(self):
+        return f"<email {self.email}>"
